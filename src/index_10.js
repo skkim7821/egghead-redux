562b50e01d3066e752ae9c8edd417c333f83aa83
skkim7821 <skkim7821@gmail.com>
2016-06-08 23:39:10 +0900

    git init;

---
+++ src/index_10.js
@@ -0,0 +1,46 @@
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+const toggleTodo = (todo) => {
+  // todo.completed = !todo.completed;
+  // return todo;
+
+  // return {
+  //   id: todo.id,
+  //   text: todo.text,
+  //   completed: !todo.completed
+  // }
+  // 
+  
+  // return Object.assign({}, todo, {
+  //   completed: !todo.completed
+  // })
+  
+  return {
+    ...todo, 
+    completed: !todo.completed
+  }
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+
+  deepFreeze(todoBefore);
+
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter)
+};
+
+testToggleTodo();
+console.log('All tests passed!');
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8090',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loaders: ['react-hot','babel']
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
